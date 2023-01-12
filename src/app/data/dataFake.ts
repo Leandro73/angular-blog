b632f984c67a4cd1b347061ff9a00f5d7b706de0
Leandro Garcia <74690946+Leandro73@users.noreply.github.com>
2023-01-11 22:19:38 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,22 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novas Notícias do Universo",
+        "description": "ola",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAnwPt5zkPMc1eWs1YeygzVGKr8xqCfPzWrw&usqp=CAU"
+    },
+
+    {
+        "id": "2",
+        "title": "Dia do Astronauta",
+        "description": "O Dia do Astronauta é comemorado anualmente em 9 de janeiro. Desde 1961, quando o astronauta russo Yuri Gagarin proclamou a famosa frase “a Terra é azul”, o ser humano dava o importante passo na história da astronomia e exploração do espaço.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Mdc4Bdm5C0bGK2KG4V6xBEdWa4kQgprPDQ&usqp=CAU"
+    },
+
+    {
+        "id": "3",
+        "title": "Empresário filantropo sorteará uma viagem ao espaço",
+        "description": "A iniciativa do bilionário jared Isaacmanpretende arrecadar fundos para as pesquisas sobre câncer infantil.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxxp11J73ReJtoHLD0i4IynB1efsXzm28b-Q&usqp=CAU"
+    }
+]
